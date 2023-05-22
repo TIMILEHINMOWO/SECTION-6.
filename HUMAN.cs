043b4fe4128a28d6f240a6c5a86d631650e9a47b
Timmy <aseoluwa05@gmail.com>
2023-05-22 13:08:07 +0100

    Add project files.

---
+++ HUMAN.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECTION_6
+{
+    internal class HUMAN
+    {
+
+        // Members
+        public string lastName;
+        public string firstName;
+        public int age;
+        public string eyeColour;
+
+        public int width;
+        public int length;
+        public int area;
+        // Property
+        public int Area
+        {
+            get { return width * length; }
+            set { area = value; }
+        }
+
+
+        //
+        public HUMAN(string lastName, string firstName, int age, string eyeColour)
+        {
+            this.age = age;
+            this.eyeColour = eyeColour;
+            this.lastName = lastName;
+            this.firstName = firstName;
+
+
+        }
+        // Public memeber method
+        public void Introduce()
+        {
+            Console.WriteLine($"My name is {firstName} {lastName}, I am {age}years old and I have {eyeColour} coloured eyes");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECTION_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+
+            HUMAN human = new HUMAN("John", " Doe", 18, "brown")
+            {
+                width = width,
+                length = length
+
+            };
+            Console.WriteLine($"Area is {human.Area}");
+
+            // Calling the method
+            human.Introduce();
+
+            Console.ReadKey();
+        }
+    }
+}
